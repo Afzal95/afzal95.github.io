4ba43f9aa9f16888f9c16f62364bbc8a8fa6631e
afzal <afzalpolite@gmail.com>
2021-07-29 22:24:06 +0530

    update icon

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-window.onload = function () {
-  // or enable responsive functionality:
-  GitHubCalendar(".calendar", "Afzal95", { responsive: true });
-}
+// window.onload = function () {
+//   // or enable responsive functionality:
+//   GitHubCalendar(".calendar", "Afzal95", { responsive: true });
+// }
